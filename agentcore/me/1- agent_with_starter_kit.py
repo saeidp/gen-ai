6ac434871466d9a18b7b8368ee2953f588b5c718
saeid pahlevan <saeid.pahlevan@curtin.edu.au>
2025-12-10 07:24:04 +0800

    agentcore: test the agent with starter kit

---
+++ agentcore/me/1- agent_with_starter_kit.py
@@ -0,0 +1,40 @@
+from bedrock_agentcore import BedrockAgentCoreApp
+from strands import Agent
+from strands.models import BedrockModel
+
+app = BedrockAgentCoreApp()
+model_id = "global.anthropic.claude-haiku-4-5-20251001-v1:0"
+
+model = BedrockModel(
+    model_id=model_id,
+)
+agent = Agent(
+    model=model,
+)
+
+@app.entrypoint
+def invoke(payload):
+    """Your AI agent function"""
+    user_message = payload.get("prompt", "Hello! How can I help you today?")
+    result = agent(user_message)
+    return {"result": result.message}
+
+if __name__ == "__main__":
+    app.run()
+
+
+#  uv run 1-agent_with_starter_kit.py
+#  Then send a POST request to http://localhost:8000/invoke with JSON body {"prompt": "Your message here"}
+
+#  Test Locally 
+#  Open another terminal and use curl to test:
+#  curl -X POST http://localhost:8080/invocations \
+#   -H "Content-Type: application/json" \
+#   -d '{"prompt": "Hello!"}'
+
+#  Configure the agent for deploying to agentcore
+#  uv run agentcore configure -e my_agent.py
+#  uv run agentcore launch
+
+#  How to test after deployment:
+#  uv run agentcore invoke '{"prompt": "tell me a joke"}' 
